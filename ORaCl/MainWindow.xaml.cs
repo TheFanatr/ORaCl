ec18376628d86d1f8f900df32b17886921202a78
Alex Fanat <alex.fanat@smus.ca>
2017-03-15 01:50:55 -0700

    first commit

--- ORaCl/MainWindow.xaml.cs
+++ ORaCl/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using System.Windows.Navigation;
 using System.Management;
 using System.Windows.Shell;
 using System.Threading;
+using WinInterop = System.Windows.Interop;
 
 namespace ORaCl
 {
@@ -30,10 +31,12 @@ namespace ORaCl
         public IntPtr Data;
         public int SizeOfData;
     }
+
     internal enum WindowCompositionAttribute
     {
         WCA_ACCENT_POLICY = 19
     }
+
     internal enum AccentState
     {
         ACCENT_DISABLED = 0,
@@ -80,116 +83,161 @@ namespace ORaCl
             return new Point(w32Mouse.X, w32Mouse.Y);
         }
 
-        Size previousWindowSize;
-        double previousWindowTop;
-        double previousWindowLeft;
-
-        bool Maximized
+        private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
-            set
+            switch (msg)
             {
-                if (!Maximized)
-                {
-                    MaximizeButton.Content = new PackIcon { Kind = PackIconKind.ChevronDown };
-                    previousWindowSize.Height = Height;
-                    previousWindowSize.Width = Width;
-                    previousWindowLeft = Left;
-                    previousWindowTop = Top;
-                    Height = SystemParameters.WorkArea.Height;
-                    Width = SystemParameters.WorkArea.Width;
-                    Left = 0;
-                    Top = 0;
-                    ResizeMode = ResizeMode.NoResize;
-                }
-                else
-                {
-                    MaximizeButton.Content = new PackIcon { Kind = PackIconKind.CropSquare };
-                    Top = previousWindowTop;
-                    Left = previousWindowLeft;
-                    Width = previousWindowSize.Width;
-                    Height = previousWindowSize.Height;
-                    ResizeMode = ResizeMode.CanResizeWithGrip;
-                }
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
             }
-            get { return ActualWidth == SystemParameters.WorkArea.Width && ActualHeight == SystemParameters.WorkArea.Height && Left == 0 && Top == 0; }
+            return (IntPtr)0;
         }
 
-        bool maximizedBeforeTabletMode;
+        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            if (monitor != IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            /// <summary>x coordinate of point.</summary>
+            public int x;
+            /// <summary>y coordinate of point.</summary>
+            public int y;
+            /// <summary>Construct a point of coordinates (x,y).</summary>
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+        
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {        
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+            public RECT rcMonitor = new RECT();
+            public RECT rcWork = new RECT();
+            public int dwFlags = 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+            public static readonly RECT Empty = new RECT();
+            public int Width { get { return Math.Abs(right - left); } }
+            public int Height { get { return bottom - top; } }
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+            public RECT(RECT rcSrc)
+            {
+                left = rcSrc.left;
+                top = rcSrc.top;
+                right = rcSrc.right;
+                bottom = rcSrc.bottom;
+            }
+            public bool IsEmpty { get { return left >= right || top >= bottom; } }
+            public override string ToString()
+            {
+                if (this == Empty) { return "RECT {Empty}"; }
+                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+            }
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
+            public override int GetHashCode() => left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
+            public static bool operator ==(RECT rect1, RECT rect2) { return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom); }
+            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
+            public static bool operator != (RECT rect1, RECT rect2) { return !(rect1 == rect2); }
+        }
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+
+        bool TabletModeActivated => (int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\ImmersiveShell", "TabletMode", 0) == 1;
 
         public MainWindow()
         {
             InitializeComponent();
+            SourceInitialized += (s, e) =>
+            {
+                IntPtr handle = (new WindowInteropHelper(this)).Handle;
+                HwndSource.FromHwnd(handle).AddHook(new HwndSourceHook(WindowProc));
+            };
             Loaded += (s, e) =>
             {
                 EnableBlur();
                 HandleTabletMode();
-                previousWindowSize = new Size(Width, Height);
                 if (WindowsIdentity.GetCurrent() != null && WindowsIdentity.GetCurrent().User != null)
                 {
                     var managementEventWatcher = new ManagementEventWatcher(new EventQuery(string.Format(@"SELECT * FROM RegistryValueChangeEvent WHERE Hive='HKEY_USERS' AND KeyPath='{0}\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\ImmersiveShell' AND ValueName='TabletMode'", WindowsIdentity.GetCurrent().User.Value)));
                     managementEventWatcher.EventArrived += (sI, eI) => Dispatcher.Invoke(() => HandleTabletMode());
                     managementEventWatcher.Start();
                 }
-                Style = (Style)Resources["TestStyle"];
-            };
-            Closed += (s, e) => globalMouseHook.Dispose();
-            StateChanged += (s, e) =>
-            {
-                switch (WindowState)
-                {
-                    case WindowState.Maximized:
-                        WindowState = WindowState.Normal;
-                        Maximized = !Maximized;
-                        break;
-                    case WindowState.Minimized:
-                        if (Width == SystemParameters.WorkArea.Width && Height == SystemParameters.WorkArea.Height)
-                        {
-                            WindowState = WindowState.Normal;
-                            Maximized = !Maximized;
-                        }
-                        break;
-                }
-            };
-            KeyDown += (sI, eI) =>
-            {
-                //if (eI.KeyboardDevice.IsKeyDown(Key.LWin) && eI.KeyboardDevice.IsKeyDown(Key.Down)) ToggleMaxmization(null, null);
-            };
-            globalMouseHook.MouseDragStartedExt += (s, e) =>
-            {
-                if (TitleBar.IsMouseOver)
-                {
-                    if (Maximized)
-                    {
-                        Maximized = !Maximized;
-                        Top = 0;
-                        Left = (GetMousePosition().X - Width) /2;
-                    }
-                    DragMove();
-                }
-            };
-            globalMouseHook.MouseDoubleClick += (s, e) =>
-            {
-                Maximized = !Maximized;
             };
             CloseButton.Click += (s, e) => Close();
-            MaximizeButton.Click += (s, e) => Maximized = !Maximized;
-            MinimizeButton.Click += (s, e) => WindowState = WindowState.Minimized;
-            void HandleTabletMode()
+            MaximizeButton.Click += (s, e) => WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+            MinimizeButton.Click += (s, e) => SystemCommands.MinimizeWindow(this);
+            StateChanged += (s, e) => Dispatcher.Invoke(() => (MaximizeButton.Content as PackIcon).Kind = WindowState == WindowState.Maximized ? PackIconKind.ChevronDown : PackIconKind.CropSquare);
+        }
+
+        void HandleTabletMode()
+        {
+            if (TabletModeActivated)
             {
-                if ((int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\ImmersiveShell", "TabletMode", 0) == 1)
-                {
-                    if (Maximized) maximizedBeforeTabletMode = true;
-                    WindowControls.Children.Remove(MinimizeButton);
-                    WindowControls.Children.Remove(MaximizeButton);
-                }
-                else if (!(WindowControls.Children.Contains(MinimizeButton)))
-                {
-                    if (!maximizedBeforeTabletMode) Maximized = !Maximized;
-                    WindowControls.Children.Insert(0, MinimizeButton);
-                    WindowControls.Children.Insert(1, MaximizeButton);
-                }
+                WindowControls.Children.Remove(MinimizeButton);
+                WindowControls.Children.Remove(MaximizeButton);
+            }
+            else if (!(WindowControls.Children.Contains(MinimizeButton)))
+            {
+                WindowControls.Children.Insert(0, MinimizeButton);
+                WindowControls.Children.Insert(1, MaximizeButton);
             }
         }
+
         internal void EnableBlur()
         {
             var windowHelper = new WindowInteropHelper(this);
